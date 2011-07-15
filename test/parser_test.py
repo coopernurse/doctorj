4eb4c6af83330d834ceaa825f1f6f763ff87e8ce
James Cooper <james@bitmechanic.com>
2011-07-15 08:35:01 -0700

    added template support.  added basic css.  added footer

--- test/parser_test.py
+++ test/parser_test.py
@@ -5,20 +5,12 @@ import unittest
 import os.path
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../src")
-from doctorj import parse_orderly_blocks
-
-def read_file(path):
-    dirname = os.path.dirname(os.path.abspath(__file__))
-    fname = os.path.join(dirname, path)
-    f = open(fname)
-    d = f.read()
-    f.close()
-    return d
+from doctorj import parse_orderly_blocks, read_file, get_rel_path
 
 class ParserTest(unittest.TestCase):
 
     def test_parse_orderly_block(self):
-        t = read_file("data/mail.md")
+        t = read_file(get_rel_path(__file__, "data/mail.md"))
         blocks = parse_orderly_blocks(t)
         self.assertEquals(2, len(blocks))
         self.assertEquals("foo_request", blocks[0][0])
